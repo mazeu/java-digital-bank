fa1cd08cb3a3d768bb6fc0bee40e27a9bfb2ead1
mazeu <marciomazeu@hotmail.com>
2021-12-15 08:11:56 -0300

    Create class ContaCorrente

---
+++ src/main/java/entity/ContaCorrente.java
@@ -0,0 +1,23 @@
+package entity;
+
+public class ContaCorrente {
+
+    private int agencia;
+    private int numero;
+    private String titular;
+    private double saldo;
+
+
+    //metodos da classe abaixo
+    public void sacar(){
+
+    }
+
+    public void depositar(){
+
+    }
+
+    public void transferir(){
+
+    }
+}
